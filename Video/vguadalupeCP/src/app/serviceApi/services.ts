d3ff362b1141e6bf502e3b5b2a2170ab5ea7106a
Adrian Guadalupe <vguadalupe@gmail.com>
2021-08-14 16:12:36 -0500

    CPFirmado

--- Video/vguadalupeCP/src/app/serviceApi/services.ts
+++
@@ -1,6 +0,0 @@
-export { AuthService } from './services/auth.service';
-export { CandidateService } from './services/candidate.service';
-export { EventService } from './services/event.service';
-export { EventAdministratorService } from './services/event-administrator.service';
-export { UserService } from './services/user.service';
-export { VoteService } from './services/vote.service';
